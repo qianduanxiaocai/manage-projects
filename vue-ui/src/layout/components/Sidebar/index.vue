76fbdf60f34c320b4fdd4485b57a36ebe3fa35de
lyl <15227936520@163.com>
2021-09-28 15:06:14 +0800

    创建项目第一次提交

---
+++ vue-ui/src/layout/components/Sidebar/index.vue
@@ -0,0 +1,118 @@
+<template>
+    <div :class="{'has-logo':showLogo}" :style="{ backgroundColor: settings.sideTheme === 'theme-dark' ? variables.menuBackground : variables.menuLightBackground }">
+        <logo v-if="showLogo" :collapse="isCollapse" />
+        <el-scrollbar :class="settings.sideTheme" wrap-class="scrollbar-wrapper">
+            <el-menu
+                :default-active="activeMenu"
+                :collapse="isCollapse"
+                :background-color="settings.sideTheme === 'theme-dark' ? variables.menuBackground : variables.menuLightBackground"
+                :text-color="settings.sideTheme === 'theme-dark' ? variables.menuColor : variables.menuLightColor"
+                :unique-opened="true"
+                :active-text-color="settings.theme"
+                :collapse-transition="false"
+                mode="vertical"
+            >
+            {{sidebarRouters}}
+                <sidebar-item
+                    v-for="(route, index) in sidebarRouters"
+                    :key="route.path  + index"
+                    :item="route"
+                    :base-path="route.path"
+                />
+            </el-menu>
+        </el-scrollbar>
+    </div>
+</template>
+
+<script>
+import { mapGetters, mapState } from "vuex";
+import Logo from "./Logo";
+import SidebarItem from "./SidebarItem";
+import variables from "@/assets/styles/variables.scss";
+
+export default {
+    components: { SidebarItem, Logo },
+    computed: {
+        ...mapState(["settings"]),
+        // ...mapGetters(["sidebarRouters","sidebar"]),
+        ...mapGetters(["sidebar"]),
+        activeMenu() {
+            const route = this.$route;
+            const { meta, path } = route;
+            // if set path, the sidebar will highlight the path you set
+            if (meta.activeMenu) {
+                return meta.activeMenu;
+            }
+            return path;
+        },
+        showLogo() {
+            return this.$store.state.settings.sidebarLogo;
+        },
+        variables() {
+            return variables;
+        },
+        isCollapse() {
+            return !this.sidebar.opened;
+        }
+    },
+    data(){
+        return{
+            sidebarRouters:[{
+                "name":"System",
+                "path":"/system",
+                "hidden":false,
+                "redirect":"noRedirect",
+                "component":"Layout",
+                "alwaysShow":true,
+                "meta":{
+                    "title":"VUE",
+                    "icon":"peoples"
+                },
+                "children":[
+                    {
+                        "name":"/departmentalWork/index",
+                        "path":"/departmentalWork/index",
+                        "component":"Layout",
+                        "meta":{
+                            "title":"部门工作",
+                            "icon":"clipboard"
+                        }
+                    },
+                ]
+            },{
+                "name":"element-ui",
+                "path":"/element-ui",
+                "redirect":"noRedirect",
+                "component":"Layout",
+                "alwaysShow":true,
+                "meta":{
+                    "title":"element-ui",
+                    "icon":"peoples"
+                },
+                "children":[
+                    {
+                        "name":"/departmentalWork/index",
+                        "path":"/departmentalWork/index",
+                        "component":"Layout",
+                        "meta":{
+                            "title":"部门工作",
+                            "icon":"clipboard"
+                        }
+                    },
+                ]
+            },{
+                    "name":"Menu",
+                    "path":"menu",
+                    "hidden":false,
+                    "component":"system/menu/index",
+                    "meta":{
+                        "title":"菜单管理",
+                        "icon":"tree-table",
+                        "noCache":false,
+                        "link":null
+                    }
+                },]
+        }
+    },
+};
+</script>
---
+++ vue-ui/src/views/index.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="app-container home">
+    <el-row :gutter="20">
+      <el-col :sm="24" :lg="24">
+        <blockquote class="text-warning" style="font-size: 14px">
+          <h3>lyl的项目管理系统</h3>
+        </blockquote>
+        <hr />
+      </el-col>
+    </el-row>
+    <el-divider />
+    <el-row :gutter="20">
+      <el-col :xs="24" :sm="24" :md="12" :lg="8">
+        <el-card class="update-log">
+          <div slot="header" class="clearfix">
+            <span>联系信息</span>
+          </div>
+          <div class="body">
+            <p>
+              <i class="el-icon-s-promotion"></i> <el-link
+                href="#"
+                target="_blank"
+                ></el-link
+              >
+            </p>
+            <p>
+              <i class="el-icon-chat-dot-round"></i> 微信：<a
+                href="javascript:;"
+                >/ *微信：</a
+              >
+            </p>
+            <p>
+              <i class="el-icon-money"></i> 支付宝：<a
+                href="javascript:;"
+                class="支付宝信息"
+                >/ *支付宝</a
+              >
+            </p>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="8">
+        <el-card class="update-log">
+          <div slot="header" class="clearfix">
+            <span>更新日志</span>
+          </div>
+          <el-collapse accordion>
+            <el-collapse-item title="v1.0.0 - 2021-09-28">
+              <ol>
+                <li>创建项目</li>
+                <li>登录与退出的路由正常跳转</li>
+              </ol>
+            </el-collapse-item>
+          </el-collapse>
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="8">
+        <el-card class="update-log">
+          <div slot="header" class="clearfix">
+            <span>支持</span>
+          </div>
+          <div class="body">
+            <img
+              src=""
+              alt="donate"
+              width="100%"
+              style="display:none"
+            />
+            <span style="display: inline-block; height: 30px; line-height: 30px"
+              >你可以请作者喝杯咖啡表示鼓励</span
+            >
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+      // 版本号
+      version: "3.7.0",
+    };
+  },
+  methods: {
+    goTarget(href) {
+      window.open(href, "_blank");
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.home {
+  blockquote {
+    padding: 10px 20px;
+    margin: 0 0 20px;
+    font-size: 17.5px;
+    border-left: 5px solid #eee;
+  }
+  hr {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    border: 0;
+    border-top: 1px solid #eee;
+  }
+  .col-item {
+    margin-bottom: 20px;
+  }
+
+  ul {
+    padding: 0;
+    margin: 0;
+  }
+
+  font-family: "open sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 13px;
+  color: #676a6c;
+  overflow-x: hidden;
+
+  ul {
+    list-style-type: none;
+  }
+
+  h4 {
+    margin-top: 0px;
+  }
+
+  h2 {
+    margin-top: 10px;
+    font-size: 26px;
+    font-weight: 100;
+  }
+
+  p {
+    margin-top: 10px;
+
+    b {
+      font-weight: 700;
+    }
+  }
+
+  .update-log {
+    ol {
+      display: block;
+      list-style-type: decimal;
+      margin-block-start: 1em;
+      margin-block-end: 1em;
+      margin-inline-start: 0;
+      margin-inline-end: 0;
+      padding-inline-start: 40px;
+    }
+  }
+}
+</style>
+
